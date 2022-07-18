e068a11166ddd3cee760b13e56000f6de36e0a37
LalaChristl <lesleyannchristl@gmail.com>
2022-07-18 17:47:32 +0200

    made changes to function

--- howManyArgs.js
+++ howManyArgs.js
@@ -1,19 +1,23 @@
 // Create a function howManyArgs which returns the total amount of arguments passed to it.
 // Example: passing 3 arguments when calling the function should return the number 3, passing 15 arguments should return the umber 15
 
-function howManyArgs () {
-console.log(arguments.length);
+// function howManyArgs () {
+// console.log(arguments.length);
 
 
-}
+// }
 
-howManyArgs(); // -> 0
-howManyArgs(1, false, "hello"); // -> 3
-howManyArgs("better"); // -> 1
+// howManyArgs(); // -> 0
+// howManyArgs(1, false, "hello"); // -> 3
+// howManyArgs("better"); // -> 1
 // howManyArgs(1, 3 , 5, 6, 7, 5, 2, 4, 4, 4, 5, 6)
+
+const howManyArgsArrow = (...args) => {
+    return args.length
+}
 
 // const howManyArgsArrow = (...args) => args.length;
 
-// console.log(howManyArgs()); // -> 0
-// console.log(howManyArgs(1, false, "hello")); // -> 3
-// console.log(howManyArgs("better")); // -> 1
+console.log(howManyArgsArrow()); // -> 0
+console.log(howManyArgsArrow(1, false, "hello")); // -> 3
+console.log(howManyArgsArrow("better")); // -> 1
